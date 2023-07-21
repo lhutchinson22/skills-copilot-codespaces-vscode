80565f58bc644e81efc4c539db573837980aff94
Lauren Blake Hutchinson <70386624+lhutchinson22@users.noreply.github.com>
2023-07-21 22:16:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS", "React", "Node"];
+    var member = {
+        name: "John",
+        age: 23,
+        skills: skills,
+        address: {
+            street: "123 Main St",
+            city: "Miami",
+            state: "FL"
+        }
+    };
+    return member;
+}
